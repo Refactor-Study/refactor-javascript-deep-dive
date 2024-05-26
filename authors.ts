77c1ecb5fb9ac5d6a6227d10196cbf1bff589178
gaeunnlee <gaeunnnleee@gmail.com>
2024-05-27 02:27:21 +0900

    chore: 작성자, 목차 정보 입력

--- authors.ts
+++ authors.ts
@@ -1,11 +1,15 @@
 type AuthorType = {
-  name: string;
-  githubId: string;
+   name: string;
+   githubId: string;
 };
 
 export const authorsInfo: Readonly<AuthorType[]> = [
-  {
-    name: '이호연',
-    githubId: 'ho991217',
-  },
+   {
+      name: '이호연',
+      githubId: 'ho991217',
+   },
+   {
+      name: '이가은',
+      githubId: 'gaeunnlee',
+   },
 ] as const;
