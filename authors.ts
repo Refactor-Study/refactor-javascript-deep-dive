7310e505a2f21710c04340b55b436fd8b0406bb7
이호연 <ho991217@kakao.com>
2024-05-27 10:06:56 +0900

    feat: 썸네일 추가

--- authors.ts
+++ authors.ts
@@ -8,4 +8,20 @@ export const authorsInfo: Readonly<AuthorType[]> = [
     name: '이호연',
     githubId: 'ho991217',
   },
+  {
+    name: '임재도',
+    githubId: 'effozen',
+  },
+  {
+    name: '구지원',
+    githubId: 'ITKOO',
+  },
+  {
+    name: '지우진',
+    githubId: 'random6-xyz',
+  },
+  {
+    name: '이가은',
+    githubId: 'gaeunnlee',
+  },
 ] as const;
